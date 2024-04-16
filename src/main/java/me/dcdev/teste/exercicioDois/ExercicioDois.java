2111a0896b8ab39762a00cc20b232253f0e2f279
Diego C. Silva <diegocaetano444@outlook.com>
2024-04-16 14:16:37 -0300

    feat: upload project

---
+++ src/main/java/me/dcdev/teste/exercicioDois/ExercicioDois.java
@@ -0,0 +1,9 @@
+package me.dcdev.teste.exercicioDois;
+
+import java.util.Scanner;
+
+public class ExercicioDois {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+    }
+}
---
+++ src/main/java/me/dcdev/teste/exercicioUm/ExercicioUm.java
@@ -0,0 +1,13 @@
+package me.dcdev.teste.exercicioUm;
+
+public class ExercicioUm {
+    public static void main(String[] args) {
+        int INDICE =13, SOMA = 0, K=0;
+
+        for(;K < INDICE;K++) {
+            SOMA = SOMA + K;
+        }
+
+        System.out.println(SOMA);
+    }
+}
